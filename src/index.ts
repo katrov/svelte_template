21fe646a8d2220bce39aa9d770a6c281af2dd837
katrov <katrov@uchi.ru>
2021-02-23 02:38:34 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,6 @@ const dffff = (val: number) => {
 
 console.log(ddd);
 dffff(45);
-dffff('45');
-dffff('45');
-dffff('45');
-dffff(45);
-dffff('45');
-dffff('45');
-dffff('45');
-dffff(45);
-dffff('45');
-dffff('45');
-dffff('45');
-
 const app = new App({
   target: document.body,
   props: { name: 'Alex' },
